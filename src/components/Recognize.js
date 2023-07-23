692176a68810451fb3ca5787a79e8785a67b2b8f
dillondesilva <dillon.desilva@geeksdiary.com>
2023-07-23 16:51:11 +1000

    Added functionality to obtain presigned URL and upload audio bytes stream

--- src/components/Recognize.js
+++ src/components/Recognize.js
@@ -2,22 +2,31 @@ import {React, Component} from 'react'
 import MicRecorder from 'mic-recorder-to-mp3';
 
 class Recognizer extends Component {
+    uploadViaPresignedURL(preSignedURL, file) {
+        fetch(preSignedURL, {
+            method: "PUT",
+            body: JSON.stringify(file)
+        }).then(response => {
+            console.log(response)
+        })
+    }
+
     processAudio(recorder) {
         console.log("Now processing audio")
-        recorder
-        .stop()
-        .getMp3().then(([buffer, blob]) => {
+        recorder.stop().getMp3().then(([buffer, blob]) => {
             // do what ever you want with buffer and blob
             // Example: Create a mp3 file and play
             const file = new File(buffer, 'sample.mp3', {
-            type: blob.type,
-            lastModified: Date.now()
+                type: blob.type,
+                lastModified: Date.now()
             });
-        
-            const player = new Audio(URL.createObjectURL(file));
-            player.play();
-            console.log(file)
-        
+
+            fetch("http://127.0.0.1:3000/get_presigned_url")
+            .then(response => response.json())
+            .then(res => {
+                this.uploadViaPresignedURL(res.preSignedURL, file);
+                console.log(res.preSignedURL)
+            })
         }).catch((e) => {
             alert('We could not retrieve your message');
             console.log(e);
