49a8ecd1c32f737db9c76bdc1abc6fe10bfa597c
Ubuntu <ubuntu@ip-172-31-22-115.ap-southeast-1.compute.internal>
2023-07-24 16:25:18 +0000

    Updated API endpoints again

--- src/components/Recognizer.js
+++ src/components/Recognizer.js
@@ -14,7 +14,7 @@ class Recognizer extends Component {
     }
 
     async recognizeChord(key) {
-        await fetch("https://chorisma.app:8080/Prod/recognize_chord", {
+        await fetch("https://chorisma.app/Prod/recognize_chord", {
             method: "POST",
             body: JSON.stringify({
                 "resourceLocation": key
@@ -36,7 +36,7 @@ class Recognizer extends Component {
             });
             console.log(file)
 
-            fetch("https://chorisma.app:8080/Prod/get_presigned_url")
+            fetch("https://chorisma.app/Prod/get_presigned_url")
             .then(response => response.json())
             .then(res => {
                 this.uploadViaPresignedURL(res.preSignedURL, file, res.key);
